8570debc2c7f77c49cb81a31e6f2e77fbc64a690
eugeniusms <eugeniusms@gmail.com>
2022-09-17 06:52:10 +0700

    Add MyWatchList Model

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,3 +1,10 @@
+from datetime import datetime
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class MyWatchList(models.Model):
+    watched = models.BooleanField(default=False)
+    title = models.CharField(default="Judul",max_length=255)
+    rating = models.IntegerField(default=1,validators=[MaxValueValidator(5), MinValueValidator(1)]) # https://stackoverflow.com/questions/849142/how-to-limit-the-maximum-value-of-a-numeric-field-in-a-django-model
+    release_date = models.DateTimeField(default=datetime.now, blank=True) # https://stackoverflow.com/questions/2771676/django-datetime-issues-default-datetime-now
+    review = models.TextField(default="Review")
