0f784f2d8c1136afe061ce10564fae45d004c3cb
eugeniusms <eugeniusms@gmail.com>
2022-09-17 08:38:38 +0700

    Change DateFieldTime => DateField (Fix Time Error)

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -6,5 +6,5 @@ class MyWatchList(models.Model):
     watched = models.BooleanField(default=False)
     title = models.CharField(default="Judul",max_length=255)
     rating = models.IntegerField(default=1,validators=[MaxValueValidator(5), MinValueValidator(1)]) # https://stackoverflow.com/questions/849142/how-to-limit-the-maximum-value-of-a-numeric-field-in-a-django-model
-    release_date = models.DateTimeField(default=datetime.now, blank=True) # https://stackoverflow.com/questions/2771676/django-datetime-issues-default-datetime-now
+    release_date = models.DateField(default=datetime.now, blank=True) # https://stackoverflow.com/questions/2771676/django-datetime-issues-default-datetime-now
     review = models.TextField(default="Review")
