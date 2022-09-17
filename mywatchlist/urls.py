8698f761acc0669500264ef0f748331fb27790ce
eugeniusms <eugeniusms@gmail.com>
2022-09-17 08:10:40 +0700

    Routing & Delivery HTML, XML, JSON

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from katalog.views import show_katalog
 
+from mywatchlist.views import show_html
+from mywatchlist.views import show_xml
+from mywatchlist.views import show_json
+
 app_name = 'watchlist'
 
 urlpatterns = [
-    path('', show_watchlist, name='show_watchlist'),
+    path('', show_html, name='show_watchlist'), # http://localhost:8000/watchlist/
+    path('html/', show_html, name='show_html'), # http://localhost:8000/watchlist/html/
+    path('xml/', show_xml, name='show_xml'), # http://localhost:8000/watchlist/xml/
+    path('json/', show_json, name='show_json'), # http://localhost:8000/watchlist/json/
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.core import serializers
+
+from mywatchlist.models import MyWatchList
+
+def show_html(request):
+    data_mywatchlist = MyWatchList.objects.all()
+    context = {
+        'list_film': data_mywatchlist,
+        'nama': 'Eugenius Mario Situmorang',
+        'npm': '2106750484'
+    }
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
