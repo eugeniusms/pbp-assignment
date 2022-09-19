e71bd375b4cdbafbf5f471256fed7201cb358a5d
eugeniusms <eugeniusms@gmail.com>
2022-09-19 21:55:57 +0700

    Add Test for MyWatchList App

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,4 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
-# class TestBlogUrls(SimpleTestCase):
+# https://codereview.stackexchange.com/questions/249173/test-django-urls
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
+# https://stackoverflow.com/questions/44160666/valueerror-missing-staticfiles-manifest-entry-for-favicon-ico
+
+class TestMyWatchListViews(TestCase):
+
+    # Test response status code show_html
+    def test_watchlist_show_html(self):         
+        response = Client().get('/mywatchlist/html/')
+        self.assertEquals(response.status_code,200)
+    
+    # Test response status code show_xml
+    def test_watchlist_show_xml(self):         
+        response = Client().get('/mywatchlist/json/')
+        self.assertEquals(response.status_code,200)
+    
+    # Test response status code show_json
+    def test_watchlist_show_json(self):         
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEquals(response.status_code,200)
