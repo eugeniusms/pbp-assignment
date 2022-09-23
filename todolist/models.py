76f20df27322e3fa1480aa3e3c2dcf20fa6d2ba9
eugeniusms <eugeniusms@gmail.com>
2022-09-23 23:02:43 +0700

    TUGAS 4: add Task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # https://docs.djangoproject.com/en/4.1/topics/auth/default/ && https://docs.djangoproject.com/en/4.1/ref/models/fields/#foreignkey
+    date = models.DateField() # https://stackoverflow.com/questions/2771676/django-datetime-issues-default-datetime-now
+    title = models.CharField(max_length=255)
+    description = models.TextField()
