d64cc4c09e0767905bb3cd895fd31642ac0e0493
eugeniusms <eugeniusms@gmail.com>
2022-09-23 23:22:40 +0700

    TUGAS 4: add login, register, and logout

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from todolist.views import todolist
+from todolist.views import todolist, login_user, register, logout_user
 
 app_name = 'todolist'
 
 urlpatterns = [
     path('', todolist, name='todolist'), # http://localhost:8000/todolist/
+    path('login/', login_user, name='login'), # http://localhost:8000/todolist/login
+    path('register/', register, name='register'), # http://localhost:8000/todolist/register
+    # path('create-task/', create_task, name='create_task'), # http://localhost:8000/todolist/create-task
+    path('logout/', logout_user, name='logout'), # http://localhost:8000/todolist/logout
 ]
